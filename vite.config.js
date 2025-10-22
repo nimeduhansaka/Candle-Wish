f74d2e408a8c4068a1cf336119756d72d450832e
nimeduhansaka <nimedu.h.gbro@gmail.com>
2025-10-22 15:11:11 +0530

    Fixed

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/Candle-Wish/',
+  build: {
+    outDir: 'docs',      // build into docs/ so Pages can serve it
+    emptyOutDir: true
+  },
   plugins: [tailwindcss(),react()],
 })
